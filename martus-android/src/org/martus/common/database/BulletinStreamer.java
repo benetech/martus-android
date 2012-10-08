06abe9ea54a42116904d35a4ca23c5195413c473
Rom Srinivasan <roms@benetech.org>
2012-10-08 13:19:24 -0700

    working on in memory Bulletin streamer

---
+++ martus-android/src/org/martus/common/database/BulletinStreamer.java
@@ -0,0 +1,31 @@
+package org.martus.common.database;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.martus.common.bulletin.Bulletin;
+import org.martus.common.crypto.MartusCrypto;
+import org.martus.util.inputstreamwithseek.ByteArrayInputStreamWithSeek;
+import org.martus.util.inputstreamwithseek.InputStreamWithSeek;
+
+/**
+ * @author roms
+ *         Date: 10/8/12
+ */
+public class BulletinStreamer implements PacketStreamOpener{
+
+    private Bulletin bulletin;
+
+
+    public BulletinStreamer(Bulletin bulletin) {
+        this.bulletin = bulletin;
+    }
+
+    @Override
+    public InputStreamWithSeek openInputStream(DatabaseKey key, MartusCrypto decrypter) throws IOException, MartusCrypto.CryptoException {
+        Writer writer = new StringWriter();
+        byte[] xml = bulletin.getBulletinHeaderPacket().writeXml(writer, decrypter);
+        return new ByteArrayInputStreamWithSeek(xml);
+    }
+}
