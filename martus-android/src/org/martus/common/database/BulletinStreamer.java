f0670df31453edefde13e347b92fc3cfe0eb100e
Rom Srinivasan <roms@benetech.org>
2012-10-08 16:01:48 -0700

    now seems to be working

--- martus-android/src/org/martus/common/database/BulletinStreamer.java
+++ martus-android/src/org/martus/common/database/BulletinStreamer.java
@@ -2,12 +2,11 @@ package org.martus.common.database;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 
 import org.martus.common.bulletin.Bulletin;
 import org.martus.common.crypto.MartusCrypto;
-import org.martus.util.inputstreamwithseek.ByteArrayInputStreamWithSeek;
 import org.martus.util.inputstreamwithseek.InputStreamWithSeek;
+import org.martus.util.inputstreamwithseek.StringInputStreamWithSeek;
 
 /**
  * @author roms
@@ -24,17 +23,17 @@ public class BulletinStreamer implements PacketStreamOpener{
 
     @Override
     public InputStreamWithSeek openInputStream(DatabaseKey key, MartusCrypto decrypter) throws IOException, MartusCrypto.CryptoException {
-        Writer writer = new StringWriter();
-        byte[] xml = new byte[0];
+        StringWriter writer = new StringWriter();
 
         if (key.getUniversalId().equals(bulletin.getBulletinHeaderPacket().getUniversalId())) {
-            xml = bulletin.getBulletinHeaderPacket().writeXml(writer, decrypter);
+            bulletin.getBulletinHeaderPacket().writeXml(writer, decrypter);
         } else if (key.getUniversalId().equals(bulletin.getFieldDataPacket().getUniversalId())) {
-            xml = bulletin.getFieldDataPacket().writeXml(writer, decrypter);
+            bulletin.getFieldDataPacket().writeXml(writer, decrypter);
         } else if (key.getUniversalId().equals(bulletin.getPrivateFieldDataPacket().getUniversalId())) {
-            xml = bulletin.getPrivateFieldDataPacket().writeXml(writer, decrypter);
+            bulletin.getPrivateFieldDataPacket().writeXml(writer, decrypter);
         }
 
-        return new ByteArrayInputStreamWithSeek(xml);
+        return new StringInputStreamWithSeek(writer.toString());
     }
+
 }
