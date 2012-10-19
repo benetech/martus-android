104625fb0359c44db184faea0bcf91e03600cc4c
Rom Srinivasan <roms@benetech.org>
2012-10-19 13:53:50 -0700

    MTM-18 seems to allow bulletins with attachment to be sent to martus

--- martus-android/src/org/martus/common/database/BulletinStreamer.java
+++ martus-android/src/org/martus/common/database/BulletinStreamer.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 import org.martus.common.bulletin.Bulletin;
+import org.martus.common.bulletin.PendingAttachmentList;
 import org.martus.common.crypto.MartusCrypto;
+import org.martus.common.packet.Packet;
 import org.martus.util.inputstreamwithseek.InputStreamWithSeek;
 import org.martus.util.inputstreamwithseek.StringInputStreamWithSeek;
 
@@ -22,18 +24,31 @@ public class BulletinStreamer implements PacketStreamOpener{
     }
 
     @Override
-    public InputStreamWithSeek openInputStream(DatabaseKey key, MartusCrypto decrypter) throws IOException, MartusCrypto.CryptoException {
+    public InputStreamWithSeek openInputStream(DatabaseKey key, MartusCrypto crypto) throws IOException, MartusCrypto.CryptoException {
         StringWriter writer = new StringWriter();
 
         if (key.getUniversalId().equals(bulletin.getBulletinHeaderPacket().getUniversalId())) {
-            bulletin.getBulletinHeaderPacket().writeXml(writer, decrypter);
+            bulletin.getBulletinHeaderPacket().writeXml(writer, crypto);
         } else if (key.getUniversalId().equals(bulletin.getFieldDataPacket().getUniversalId())) {
-            bulletin.getFieldDataPacket().writeXml(writer, decrypter);
+            bulletin.getFieldDataPacket().writeXml(writer, crypto);
         } else if (key.getUniversalId().equals(bulletin.getPrivateFieldDataPacket().getUniversalId())) {
-            bulletin.getPrivateFieldDataPacket().writeXml(writer, decrypter);
+            bulletin.getPrivateFieldDataPacket().writeXml(writer, crypto);
+        } else {
+            //write attachments
+            writePendingAttachments(bulletin.getPendingPublicAttachments(), writer, crypto);
+            writePendingAttachments(bulletin.getPendingPrivateAttachments(), writer, crypto);
         }
 
         return new StringInputStreamWithSeek(writer.toString());
     }
+
+    private static void writePendingAttachments(PendingAttachmentList pendingAttachments, StringWriter writer, MartusCrypto crypto) throws IOException, MartusCrypto.CryptoException
+    	{
+    		for(int i = 0; i < pendingAttachments.size(); ++i)
+    		{
+    			Packet packet = pendingAttachments.get(i);
+                packet.writeXml(writer, crypto);
+    		}
+    	}
 
 }
