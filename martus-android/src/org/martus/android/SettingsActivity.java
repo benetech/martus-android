8ded6310efbb9e1aa332be27a2e897b0445b6b75
Rom Srinivasan <roms@benetech.org>
2012-12-11 15:03:11 -0800

    minor menu and settings re-ordering

--- martus-android/src/org/martus/android/SettingsActivity.java
+++ martus-android/src/org/martus/android/SettingsActivity.java
@@ -4,10 +4,13 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
 /**
  * @author roms
@@ -28,6 +31,9 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getActionBar();
+                actionBar.setDisplayHomeAsUpEnabled(true);
 
         languageNamesArray = getResources().getStringArray(R.array.entries_language_preference);
         languageCodesArray = getResources().getStringArray(R.array.values_language_preference);
@@ -76,6 +82,20 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
         super.onPause();
         getPreferenceScreen().getSharedPreferences()
                 .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, MartusActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
