3f0c1257d60dee58416222ce23ea0ef296a67d88
Rom Srinivasan <roms@benetech.org>
2012-12-12 16:08:46 -0800

    overriding accidental check in

--- martus-android/src/org/martus/android/MartusApplication.java
+++ martus-android/src/org/martus/android/MartusApplication.java
@@ -1,46 +1,12 @@
 package org.martus.android;
 
-import org.martus.common.crypto.MartusSecurity;
-
 import android.app.Application;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 
 /**
  * @author roms
  *         Date: 10/24/12
  */
 public class MartusApplication extends Application {
-
-    public static final long DISCONNECT_TIMEOUT = 120000; // 2 min = 5 * 60 * 1000 ms
-
-    private Handler disconnectHandler = new Handler(){
-        public void handleMessage(Message msg) {
-        }
-    };
-
-    private Runnable disconnectCallback = new Runnable() {
-        @Override
-        public void run() {
-            Log.e(AppConfig.LOG_LABEL, " SHOULD  Auto Session Logout !!!!!!!!");
-            // Perform any required operation on disconnect
-            MartusSecurity martusCrypto = AppConfig.getInstance().getCrypto();
-            if (null != martusCrypto) {
-                //martusCrypto.clearKeyPair();
-                Log.e(AppConfig.LOG_LABEL, "Auto Session Logout !!!!!!!!");
-            }
-        }
-    };
-
-    public void resetDisconnectTimer(){
-        disconnectHandler.removeCallbacks(disconnectCallback);
-        disconnectHandler.postDelayed(disconnectCallback, DISCONNECT_TIMEOUT);
-    }
-
-    public void stopDisconnectTimer(){
-        disconnectHandler.removeCallbacks(disconnectCallback);
-    }
 
     @Override
     public void onCreate()
@@ -50,7 +16,6 @@ public class MartusApplication extends Application {
         // Initialize the singletons so their instances
         // are bound to the application process.
         initSingletons();
-        resetDisconnectTimer();
     }
 
     protected void initSingletons()
