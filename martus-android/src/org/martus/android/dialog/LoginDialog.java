55c22952574284a796a06263e86139b968b9b5e8
Rom Srinivasan <roms@benetech.org>
2013-01-04 17:40:16 -0800

    refactored out anonymous inner class

--- martus-android/src/org/martus/android/dialog/LoginDialog.java
+++ martus-android/src/org/martus/android/dialog/LoginDialog.java
@@ -7,8 +7,11 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,7 +19,11 @@ import android.widget.TextView;
  * @author roms
  *         Date: 12/22/12
  */
-public class LoginDialog extends DialogFragment {
+public class LoginDialog extends DialogFragment implements DialogInterface.OnClickListener, TextView.OnEditorActionListener {
+
+    private EditText passwordText;
+
+
 
     public interface LoginDialogListener {
         void onFinishPasswordDialog(TextView inputText);
@@ -41,27 +48,35 @@ public class LoginDialog extends DialogFragment {
 
         LayoutInflater factory = LayoutInflater.from(getActivity());
         View passwordEntryView = factory.inflate(R.layout.password_dialog, null);
-        final EditText passwordText = (EditText) passwordEntryView.findViewById(R.id.password_edit);
+        passwordText = (EditText) passwordEntryView.findViewById(R.id.password_edit);
+        passwordText.setOnEditorActionListener(this);
 
         return new AlertDialog.Builder(getActivity())
             .setIcon(android.R.drawable.ic_dialog_alert)
             .setTitle(R.string.password_dialog_title)
             .setView(passwordEntryView)
-            .setPositiveButton(R.string.alert_dialog_ok,
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                            ((LoginDialogListener) getActivity()).onFinishPasswordDialog(passwordText);
-                        }
-                    }
-            )
-            .setNegativeButton(R.string.password_dialog_cancel,
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                            ((LoginDialogListener) getActivity()).onCancelPasswordDialog();
-                        }
-                    }
-            )
+            .setPositiveButton(R.string.alert_dialog_ok, this)
+            .setNegativeButton(R.string.password_dialog_cancel, this)
             .create();
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            ((LoginDialogListener) getActivity()).onFinishPasswordDialog(passwordText);
+            this.dismiss();
+            return true;
+        }
+        return false;
+    }
+
+    public void onClick(DialogInterface dialog, int whichButton) {
+        switch (whichButton) {
+            case -1:    ((LoginDialogListener) getActivity()).onFinishPasswordDialog(passwordText);
+                        break;
+            case -2:    ((LoginDialogListener) getActivity()).onCancelPasswordDialog();
+                        break;
+        }
     }
 
 }
