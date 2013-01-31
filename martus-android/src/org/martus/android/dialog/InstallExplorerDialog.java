be8ce38f0cf6bbb19fba4e5333c315c507175522
Rom Srinivasan <roms@benetech.org>
2013-01-31 10:32:36 -0800

    added okay button to install file explorer dialog to be consistent with other dialogs.

--- martus-android/src/org/martus/android/dialog/InstallExplorerDialog.java
+++ martus-android/src/org/martus/android/dialog/InstallExplorerDialog.java
@@ -4,6 +4,7 @@ import org.martus.android.R;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -12,7 +13,7 @@ import android.view.LayoutInflater;
  * @author roms
  *         Date: 12/23/12
  */
-public class InstallExplorerDialog extends DialogFragment {
+public class InstallExplorerDialog extends DialogFragment implements DialogInterface.OnClickListener {
 
     public InstallExplorerDialog() {
         // Empty constructor required for DialogFragment
@@ -31,7 +32,13 @@ public class InstallExplorerDialog extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
             .setIcon(android.R.drawable.ic_dialog_alert)
             .setView(inflater.inflate(R.layout.install_file_explorer, null))
-            .setTitle("Try another file explorer")
+            .setTitle(R.string.install_file_explorer)
+            .setPositiveButton(R.string.alert_dialog_ok,this)
             .create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        //do nothing
     }
 }
